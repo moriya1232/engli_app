710cfa907e8d8afa8513ad91977cb98ba8c3abac
shilo leopold <shiloleopold@gmail.om>
2021-04-08 16:55:43 +0300

    shilo1

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A new Flutter application.
 
-## Getting Started
+## Getting Startedre
 
 This project is a starting point for a Flutter application.
 
---
+++ lib/srevices/auth.dart
@@ -0,0 +1,31 @@
+import 'package:engli_app/models/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+//creat new user that based om the firebase user
+  MyUser _createUser(User user) {
+    if (user != null) {
+      return MyUser(uniqeID: user.uid);
+    } else {
+      return null;
+    }
+  }
+
+  //stream to know if memeber login/logout
+  Stream<User> get user {
+    return _auth.authStateChanges();
+  }
+
+  //sign in anonymously
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
