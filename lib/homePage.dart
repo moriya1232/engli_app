9d57430b0f9fc3378c7ee9026858b179f9ef96c6
moriya1232 <moriya1232@gmail.com>
2021-04-12 02:32:49 +0300

    start to check animations. dont work yet.

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -34,92 +34,110 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Padding(
               padding: EdgeInsets.symmetric(vertical: 30),
-              child: Text(
-                '!ברוכים הבאים',
-                style: TextStyle(
-                  fontFamily: 'Abraham-h',
-                  fontSize: 40,
-                ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //TODO : animated letters.
+                  AnimationWidget(),
+                  AnimationWidget(),
+//                  AnimatedContainer(
+//                      child: Image.asset('images/A.jpg'),
+//                      duration: Duration(seconds: 1)),
+//                  AnimatedContainer(
+//                      child: Image.asset('images/R.jpg'),
+//                      duration: Duration(seconds: 1)),
+//                  AnimatedContainer(
+//                      child: Image.asset('images/F.jpg'),
+//                      duration: Duration(seconds: 1)),
+//                  AnimatedContainer(
+//                      child: Image.asset('images/W.jpg'),
+//                      duration: Duration(seconds: 1)),
+                ],
               ),
             ),
-            Column(children: [
-              ButtonTheme(
-                  buttonColor: Colors.black87,
-                  child: SizedBox(
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '!ברוכים הבאים',
+                    style: TextStyle(
+                      fontFamily: 'Abraham-h',
+                      fontSize: 40,
+                    ),
+                  ),
+                  Column(children: [
+                    SizedBox(
+                        height: 70,
+                        width: 220,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(22.0)),
+                            primary: Colors.amberAccent,
+                          ),
+                          onPressed: () {
+                            registerationClicked();
+                          },
+                          child: Text('הרשמה',
+                              style: TextStyle(
+                                  fontFamily: 'Comix-h',
+                                  color: Colors.black87,
+                                  fontSize: 30)),
+                        )),
+                    SizedBox(height: 20),
+                    SizedBox(
                       height: 70,
                       width: 220,
                       child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(22.0)),
-                          primary: Colors.amberAccent,
-                        ),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0)),
+                            primary: Colors.amberAccent),
                         onPressed: () {
-                          registerationClicked();
+                          loginClicked();
                         },
-                        child: Text('הרשמה',
-                            style: TextStyle(
-                                fontFamily: 'Comix-h',
-                                color: Colors.black87,
-                                fontSize: 30)),
-                      ))),
-              SizedBox(height: 20),
-              ButtonTheme(
-                buttonColor: Colors.amberAccent,
-                child: SizedBox(
-                  height: 70,
-                  width: 220,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0)),
-                        primary: Colors.amberAccent
+                        child: Text(
+                          'התחברות',
+                          style: TextStyle(
+                              fontFamily: 'Comix-h',
+                              color: Colors.black87,
+                              fontSize: 30),
+                        ),
+                      ),
                     ),
-                    onPressed: () {
-                      loginClicked();
-                    },
-                    child: Text(
-                      'התחברות',
-                      style: TextStyle(
-                          fontFamily: 'Comix-h',
-                          color: Colors.black87,
-                          fontSize: 30),
+                    SizedBox(height: 20),
+                    SizedBox(
+                      height: 50,
+                      width: 140,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0)),
+                            primary: Colors.amberAccent),
+                        onPressed: () {
+                          loginGuestClicked();
+                        },
+                        child: Text(
+                          'התחבר כאורח',
+                          style: TextStyle(
+                              fontFamily: 'Comix-h',
+                              color: Colors.black87,
+                              fontSize: 15),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              ButtonTheme(
-                buttonColor: Colors.amberAccent,
-                child: SizedBox(
-                  height: 50,
-                  width: 140,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0)),
-                        primary: Colors.amberAccent
-                    ),
-                    onPressed: () {
-                      loginGuestClicked();
-                    },
-                    child: Text(
-                      'התחבר כאורח',
-                      style: TextStyle(
-                          fontFamily: 'Comix-h',
-                          color: Colors.black87,
-                          fontSize: 15),
+                  ]),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Image(
+                      image: AssetImage('images/kids-read.jpg'),
                     ),
                   ),
-                ),
+                ],
               ),
-            ]),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Image(
-                image: AssetImage('images/kids-read.jpg'),
-              ),
-            )
+            ),
           ]),
     );
   }
@@ -140,6 +158,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void loginGuestClicked() {
     //TODO
+  }
+}
 
+class AnimationWidget extends StatefulWidget {
+  @override
+  _AnimationWidgetState createState() => _AnimationWidgetState();
+}
+
+class _AnimationWidgetState extends State<AnimationWidget>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Image.asset('images/A.jpg');
   }
 }
